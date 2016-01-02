07093f1166e93745cca9cd5e276946643a0b43e8
Uli Kusterer <ulivc@zathras.de>
2016-01-02 23:01:36 +0100

    Initial Commit

---
+++ CutTheCord/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CutTheCord
+//
+//  Created by Uli Kusterer on 02/01/16.
+//  Copyright © 2016 Uli Kusterer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
