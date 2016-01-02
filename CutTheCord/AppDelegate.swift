d865d7ed0dacf89498f71f3df4fe561ccf81ce7e
Uli Kusterer <ulivc@zathras.de>
2016-01-03 00:56:28 +0100

    Initial checkin.

--- CutTheCord/AppDelegate.swift
+++ CutTheCord/AppDelegate.swift
@@ -7,21 +7,30 @@
 //
 
 import Cocoa
+import WebKit
+
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 	@IBOutlet weak var window: NSWindow!
-
+	@IBOutlet weak var webView: WKWebView!
 
 	func applicationDidFinishLaunching(aNotification: NSNotification) {
 		// Insert code here to initialize your application
+		
+		window.styleMask = NSBorderlessWindowMask | NSResizableWindowMask | NSTexturedBackgroundWindowMask
+		window.level = Int(CGWindowLevelForKey(CGWindowLevelKey.PopUpMenuWindowLevelKey))
+//		var	box : NSRect = window.frame
+//		box.size = NSScreen.screens()![0].frame.size
+//		window.setFrame( box, display: true )
+		
+		NSApplication.sharedApplication().presentationOptions = [NSApplicationPresentationOptions.AutoHideDock, NSApplicationPresentationOptions.AutoHideMenuBar]
+		
+		let	osVersion = NSProcessInfo.processInfo().operatingSystemVersion
+		webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X \(osVersion.majorVersion)_\(osVersion.minorVersion)_\(osVersion.patchVersion)) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"
+		let	pageURL = NSURL(string:"http://netflix.com")
+		webView.loadRequest( NSURLRequest( URL: pageURL! ) )
 	}
-
-	func applicationWillTerminate(aNotification: NSNotification) {
-		// Insert code here to tear down your application
-	}
-
-
 }
 
