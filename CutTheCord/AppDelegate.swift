3d6f48ee328133842dc3528a8901420ee6db7b37
Uli Kusterer <ulivc@zathras.de>
2016-01-03 04:38:36 +0100

    Add some convenient standard sizes to the View menu.

--- CutTheCord/AppDelegate.swift
+++ CutTheCord/AppDelegate.swift
@@ -21,9 +21,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		
 		window.styleMask = NSBorderlessWindowMask | NSResizableWindowMask | NSTexturedBackgroundWindowMask
 		window.level = Int(CGWindowLevelForKey(CGWindowLevelKey.PopUpMenuWindowLevelKey))
-//		var	box : NSRect = window.frame
-//		box.size = NSScreen.screens()![0].frame.size
-//		window.setFrame( box, display: true )
 		
 		NSApplication.sharedApplication().presentationOptions = [NSApplicationPresentationOptions.AutoHideDock, NSApplicationPresentationOptions.AutoHideMenuBar]
 		
@@ -31,6 +28,48 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X \(osVersion.majorVersion)_\(osVersion.minorVersion)_\(osVersion.patchVersion)) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"
 		let	pageURL = NSURL(string:"http://netflix.com")
 		webView.loadRequest( NSURLRequest( URL: pageURL! ) )
+	}
+	
+	@IBAction func placeWindowInLowerLeft( sender : AnyObject? ) {
+		var	box : NSRect = window.frame
+		box = NSScreen.screens()![0].frame
+		box.size.width /= 4.0
+		box.size.height /= 4.0
+		window.setFrame( box, display: true )
+	}
+	
+	@IBAction func placeWindowInLowerRight( sender : AnyObject? ) {
+		var	box : NSRect = window.frame
+		box = NSScreen.screens()![0].frame
+		box.origin.x += (box.size.width / 4) * 3
+		box.size.width /= 4.0
+		box.size.height /= 4.0
+		window.setFrame( box, display: true )
+	}
+	
+	@IBAction func placeWindowInUpperRight( sender : AnyObject? ) {
+		var	box : NSRect = window.frame
+		box = NSScreen.screens()![0].frame
+		box.origin.x += (box.size.width / 4) * 3
+		box.size.width /= 4.0
+		box.origin.y += (box.size.height / 4) * 3
+		box.size.height /= 4.0
+		window.setFrame( box, display: true )
+	}
+	
+	@IBAction func placeWindowInUpperLeft( sender : AnyObject? ) {
+		var	box : NSRect = window.frame
+		box = NSScreen.screens()![0].frame
+		box.size.width /= 4.0
+		box.origin.y += (box.size.height / 4) * 3
+		box.size.height /= 4.0
+		window.setFrame( box, display: true )
+	}
+	
+	@IBAction func placeWindowToFillScreen( sender : AnyObject? ) {
+		var	box : NSRect = window.frame
+		box = NSScreen.screens()![0].frame
+		window.setFrame( box, display: true )
 	}
 }
 
